1b7289607606690f4cb7365332acbfd3dea4e481
Xx-VitorBigV-xX <129459463+Xx-VitorBigV-xX@users.noreply.github.com>
2023-05-01 20:08:37 -0300

    sprint 2.0

--- app/src/main/java/com/example/telacadastro/Emergencia.kt
+++ app/src/main/java/com/example/telacadastro/Emergencia.kt
@@ -4,18 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.telacadastro.databinding.ActivityEmergenciaBinding
-import com.example.telacadastro.databinding.ActivityMain2Binding
 
-class MainActivity2 : AppCompatActivity() {
-    private lateinit var binding:ActivityMain2Binding
+class Emergencia : AppCompatActivity() {
+
+    private lateinit var binding : ActivityEmergenciaBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMain2Binding.inflate(layoutInflater)
+        binding = ActivityEmergenciaBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.BtnEmergencia.setOnClickListener { btnemerg -> iremergencia() }
     }
 
 
