c6a3abe528620f12fc09c737a79475905e140bae
Xx-VitorBigV-xX <129459463+Xx-VitorBigV-xX@users.noreply.github.com>
2023-05-01 19:17:10 -0300

    sprint2

--- app/src/main/java/com/example/telacadastro/telaLogin.kt
+++ app/src/main/java/com/example/telacadastro/telaLogin.kt
@@ -1,11 +1,39 @@
 package com.example.telacadastro
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.telacadastro.databinding.TelaLoginBinding
 
 class telaLogin:AppCompatActivity() {
+
+    private lateinit var binding: TelaLoginBinding
+
+
     override fun onCreate(savedInstanceState : Bundle?){
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tela_login)
+        binding = TelaLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.cadastro.setOnClickListener{tvcadastro-> ircadastro()}
+
+        binding.BtnEntar.setOnClickListener {btn -> irTelaPerfil()}
     }
+
+
+
+
+
+
+    private fun ircadastro(){
+        var telacadastro = Intent(this,criarConta::class.java)
+        startActivity(telacadastro)
+    }
+
+
+    private fun irTelaPerfil(){
+        var telaperfil = Intent(this,MainActivity2::class.java)
+        startActivity(telaperfil)
+    }
+
 }
