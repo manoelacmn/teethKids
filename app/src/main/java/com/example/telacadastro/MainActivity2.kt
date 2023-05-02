ea9187292f066b15044da8a9ed8db4e1a26f5b84
Xx-VitorBigV-xX <129459463+Xx-VitorBigV-xX@users.noreply.github.com>
2023-05-02 17:23:05 -0300

    sprint 2 switch,autenticação

--- app/src/main/java/com/example/telacadastro/MainActivity2.kt
+++ app/src/main/java/com/example/telacadastro/MainActivity2.kt
@@ -3,10 +3,13 @@ package com.example.telacadastro
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.telacadastro.databinding.ActivityEmergenciaBinding
 import com.example.telacadastro.databinding.ActivityMain2Binding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     private lateinit var binding:ActivityMain2Binding
 
 
@@ -15,10 +18,33 @@ class MainActivity2 : AppCompatActivity() {
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.BtnEmergencia.setOnClickListener { btnemerg -> iremergencia() }
+        binding.switch1.setOnCheckedChangeListener{buttonView,isCheked -> isCheked
+        var ligadoOuDesligado = ""
+        if (isCheked) {
+            ligadoOuDesligado = "Ligado"
+
+            val status = binding.switch1.text.toString()
+            statustperfil(status)
+
+            Toast.makeText(
+                baseContext,
+                "Online",
+                Toast.LENGTH_SHORT,
+            ).show()
+        }else {
+            ligadoOuDesligado="Desligado"
+            Toast.makeText(
+                baseContext,
+                "Offline",
+                Toast.LENGTH_SHORT,
+            ).show()
+        }
+        binding.switch1.text = ligadoOuDesligado
+        }
     }
 
-
+    private fun statustperfil(status:String) {
+    }
 
     private fun iremergencia(){
         var irParaEmerg = Intent(this,Emergencia::class.java)
