f62cbe625062f5462dcd73983de1314bd7d0e373
manoelacmn <manoel.acmn@puccampinas.edu.br>
2023-05-04 17:35:55 -0300

    preparation for FCM notifications feature

--- app/src/main/java/com/example/telacadastro/MainActivity.kt
+++ app/src/main/java/com/example/telacadastro/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.telacadastro
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import com.example.telacadastro.databinding.ActivityMainBinding
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
@@ -37,6 +42,24 @@ class MainActivity : AppCompatActivity() {
         startActivity(telaLog)
     }
 
+
+//    private fun createNotificationChannel() {
+//        // Create the NotificationChannel, but only on API 26+ because
+//        // the NotificationChannel class is new and not in the support library
+//        val name = getString(R.string.channel_name)
+//        val descriptionText = getString(R.string.channel_description)
+//        val importance = NotificationManager.IMPORTANCE_DEFAULT
+//        val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+//            description = descriptionText
+//        }
+//        // Register the channel with the system
+//        val notificationManager: NotificationManager =
+//            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+//        notificationManager.createNotificationChannel(channel)
+//    }
+
+
+    // Declare the launcher at the top of your Activity/Fragment:
     private val requestPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestPermission(),
     ) { isGranted: Boolean ->
@@ -47,21 +70,19 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-//    private fun askNotificationPermission() {
-//        // This is only necessary for API level >= 33 (TIRAMISU)
-//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
-//            PackageManager.PERMISSION_GRANTED
-//        ) {
-//            // FCM SDK (and your app) can post notifications.
-//        } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
-//            // TODO: display an educational UI explaining to the user the features that w4ill be enabled
-//            //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
-//            //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
-//            //       If the user selects "No thanks," allow the user to continue without notifications.
-//        } else {
-//            // Directly ask for the permission
-//            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
-//        }
-//
-//    }
+    fun runtimeEnableAutoInit() {
+        // [START fcm_runtime_enable_auto_init]
+        Firebase.messaging.isAutoInitEnabled = true
+        // [END fcm_runtime_enable_auto_init]
+    }
+
+    var builder = NotificationCompat.Builder(this, "emergencyAlert")
+        .setSmallIcon(R.drawable.heart_circle_plus_solid)
+        .setContentTitle("nova emergência")
+        .setContentText("aceitar emergência?")
+        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+//        .addAction(R.drawable.ic_snooze, getString(R.string.snooze),
+//            snoozePendingIntent)
+
+
 }
