41186f553355459704385cf8825bab62d636a155
Gabriel <gabrielbsornas@gmail.com>
2023-05-04 23:16:33 -0300

    atualização de dados

--- app/src/main/java/com/example/telacadastro/MainActivity2.kt
+++ app/src/main/java/com/example/telacadastro/MainActivity2.kt
@@ -3,11 +3,14 @@ package com.example.telacadastro
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import com.example.telacadastro.databinding.ActivityCriarcontaBinding
 import com.example.telacadastro.databinding.ActivityEmergenciaBinding
 import com.example.telacadastro.databinding.ActivityMain2Binding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class MainActivity2 : AppCompatActivity() {
@@ -80,5 +83,42 @@ class MainActivity2 : AppCompatActivity() {
 
         private val TAG = "myProfile"
 
+    }
+    private fun hideSoftKeyboard(){
+        val softKeyManager =
+            getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        softKeyManager.hideSoftInputFromWindow(binding.btnSalvarDados.windowToken, 0)
+    }
+
+
+    private fun onCreate(savedInstanceState: Bundle?, optionalParam: String? = null) {
+        auth = Firebase.auth
+        super.onCreate(savedInstanceState)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSalvarDados.setOnClickListener { btnSalvarDados ->
+            hideSoftKeyboard()
+            val email = binding.etEmailEditarPerfil.text.toString()
+            val nome = binding.etNomeEditarPerfil.text.toString()
+            val endereco1 = binding.etEndereco1EditarPerfil.text.toString()
+            val endereco2 = binding.etEndereco2EditarPerfil.text.toString()
+            val endereco3 = binding.etEndereco3EditarPerfil.text.toString()
+            val cv = binding.etCurriculoEditarPerfil.text.toString()
+
+            //singUpNewAccount(nome , email)
+            val db = Firebase.firestore
+//
+            val usuario = hashMapOf(
+                "email" to email,
+                "nome" to nome,
+                "endereco1" to endereco1,
+                "endereco2" to endereco2,
+                "endereco3" to endereco3,
+                "curriculo" to cv
+            )
+            db.collection("usuarios").add(usuario)
+            Toast.makeText(baseContext,"Atualizado",Toast.LENGTH_LONG).show()
+        }
     }
 }
