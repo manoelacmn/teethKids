8c62538987fc751fe49928514dddaf11a2a27c11
Xx-VitorBigV-xX <129459463+Xx-VitorBigV-xX@users.noreply.github.com>
2023-05-11 17:45:21 -0300

    upload imagem storage

--- app/src/main/java/com/example/telacadastro/Emergencia.kt
+++ app/src/main/java/com/example/telacadastro/Emergencia.kt
@@ -1,13 +1,17 @@
 package com.example.telacadastro
 
+import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import com.example.telacadastro.databinding.ActivityEmergenciaBinding
+import com.google.firebase.firestore.util.Util
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
@@ -23,6 +27,7 @@ class Emergencia : AppCompatActivity() {
         binding = ActivityEmergenciaBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.btnEnviar.setOnClickListener{btnir ->
+            uploadimage()
         }
         storage= Firebase.storage
     }
@@ -31,11 +36,32 @@ class Emergencia : AppCompatActivity() {
         menuInflater.inflate(R.menu.menu,menu)
         return super.onCreateOptionsMenu(menu)
     }
+    private fun obterImagemGaleria(){
+        val intent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+        startActivityForResult(Intent.createChooser(intent,"escolha uma imagem"),11)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode==Activity.RESULT_OK){
+            if(requestCode == 11){
+                if(data != null){
+                    val uri = data.data
+                    binding.uploadImageView.setImageURI(uri)
+                }
+            }
+        }
+    }
+
+
+
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when(item.itemId){
-            R.id.action_man -> {
-                println("main menu clicado")
+            R.id.action_submenu2 -> {
+                obterImagemGaleria()
                 true
             }
             else ->{
@@ -60,8 +86,10 @@ class Emergencia : AppCompatActivity() {
         val reference = storage!!.reference.child("imagens").child("uploadimage.png")
         var uploadTask = reference.putBytes(data)
         uploadTask.addOnFailureListener {
+            Toast.makeText(baseContext,"Erro",Toast.LENGTH_SHORT).show()
             // Handle unsuccessful uploads
         }.addOnSuccessListener { taskSnapshot ->
+            Toast.makeText(baseContext,"Sucesso",Toast.LENGTH_SHORT).show()
             // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
             // ...
         }
