49f2d4c318cc61c61eb874398e46ed00944f1bbb
manoelacmn <manoel.acmn@puccampinas.edu.br>
2023-06-15 22:03:05 -0300

    added GSON to Tela_Inicial.kt

--- app/src/main/java/com/example/telacadastro/Tela_Inicial.kt
+++ app/src/main/java/com/example/telacadastro/Tela_Inicial.kt
@@ -10,13 +10,31 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import io.grpc.Internal
+import com.google.gson.Gson
 
+data class Data(
+    val senha: String?,
+    val uid: String?,
+    val current: Current?,
+    val telefone: String?,
+    val endereco: String?,
+    val curriculo: String?,
+    val nome: String?,
+    val email: String?,
+    val fcmtoken: String?,
+    val enderecos: List<String>?,
+    val status: String?
+)
+
+data class Current(
+    val emergencyPATH: String?
+)
 
 private lateinit var binding: ActivityTelaInicialBinding
 
 class Tela_Inicial : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+    private val gson = Gson()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val db = Firebase.firestore
@@ -27,12 +45,10 @@ class Tela_Inicial : AppCompatActivity() {
             val uid = it.uid
         }
         super.onCreate(savedInstanceState)
-        binding= ActivityTelaInicialBinding.inflate(layoutInflater)
+        binding = ActivityTelaInicialBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        Log.d("userUID",user!!.uid)
-
+        Log.d("userUID", user!!.uid)
 
         db.collection("usuarios")
             .whereEqualTo("uid", user!!.uid)
@@ -40,53 +56,54 @@ class Tela_Inicial : AppCompatActivity() {
             .addOnSuccessListener { documents ->
                 for (document in documents) {
                     Log.d(ContentValues.TAG, "${document.id} => ${document.data}")
-                    binding.tvTeethKids.text = document.data["nome"].toString()
+                    val dataString = gson.toJson(document.data)
+                    val jsonData: Data = gson.fromJson(dataString, Data::class.java)
+                    binding.tvTeethKids.text = jsonData.nome ?: ""
                 }
             }
             .addOnFailureListener { exception ->
                 Log.w(ContentValues.TAG, "Error getting documents: ", exception)
             }
 
-
-
-        binding.btnTelaPerfil.setOnClickListener{btnir ->
+        binding.btnTelaPerfil.setOnClickListener { btnir ->
             irTelaperfil()
         }
         binding.btnTelaEmergencia.setOnClickListener { btnir ->
             emergencia()
         }
-        binding.btnConfiguraO.setOnClickListener({btn -> configuracao()})
-        binding.btnAvaliaO.setOnClickListener{btn->avaliacao()}
-        binding.btnHistorico.setOnClickListener { btn->historico() }
-        binding.btnLog.setOnClickListener{btn->atual()}
-
-
-
-
+        binding.btnConfiguraO.setOnClickListener({ btn -> configuracao() })
+        binding.btnAvaliaO.setOnClickListener { btn -> avaliacao() }
+        binding.btnHistorico.setOnClickListener { btn -> historico() }
+        binding.btnLog.setOnClickListener { btn -> atual() }
     }
 
-    private fun irTelaperfil(){
-        var irPerfil = Intent(this,PerfilActivity::class.java)
+    private fun irTelaperfil() {
+        val irPerfil = Intent(this, PerfilActivity::class.java)
         startActivity(irPerfil)
     }
-    private fun emergencia(){
-        var irEmergencia = Intent(this,recycleVeiw::class.java)
+
+    private fun emergencia() {
+        val irEmergencia = Intent(this, recycleVeiw::class.java)
         startActivity(irEmergencia)
     }
-    private fun configuracao(){
-        var intent=Intent(this,Emergencia::class.java)
+
+    private fun configuracao() {
+        val intent = Intent(this, Emergencia::class.java)
         startActivity(intent)
     }
-    private fun avaliacao(){
-        var intent=Intent(this,recycleravaliacao::class.java)
+
+    private fun avaliacao() {
+        val intent = Intent(this, recycleravaliacao::class.java)
         startActivity(intent)
     }
-    private fun historico(){
-        var intent=Intent(this,recyclerHistorico::class.java)
+
+    private fun historico() {
+        val intent = Intent(this, recyclerHistorico::class.java)
         startActivity(intent)
     }
-    private fun atual(){
-        val intent = Intent(this,segundaConfirmacao::class.java)
+
+    private fun atual() {
+        val intent = Intent(this, segundaConfirmacao::class.java)
         startActivity(intent)
     }
 }
